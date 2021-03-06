89812db4a39cecfe41ed0a476b9a6c6a1f8057d1
Francesco Scassillo <scassillofrancesco@gmail.com>
2021-03-06 14:27:46 +0100

    Documentation

--- src/database/transaction/transaction.interface.ts
+++ src/database/transaction/transaction.interface.ts
@@ -1,10 +1,11 @@
 /**
- * @description Transaction interface
+ * A transaction is a data block to push in the network
  * @param timestamp  long format of the date time of the transaction.
  * @param body the json data of the transaction
  * @param owner transaction owner public key
+ * @param recepient if null to the network
  * @param protocol an identifier to describe the kind of transaction
- * @param signatures list of signatures of the history of the content form the owner  
+ * @param signature signature of the the owner  
  */
 export interface ITransaction {
     timestamp: number;
