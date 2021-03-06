83a9492b6e92724735e671f60b6ab1f32cfc0568
Francesco Scassillo <scassillofrancesco@gmail.com>
2021-03-06 14:06:51 +0100

    Make transaction service work

--- src/database/transaction/transaction.service.ts
+++ src/database/transaction/transaction.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ITransaction } from './transaction.interface';
 import axios from 'axios';
-import { readdir } from 'fs';
+import { readdirSync } from 'fs';
 import { Utilty } from '../utility';
 import * as nodes from '../../data/nodes.list.json';
 
@@ -15,37 +15,27 @@ export class TransactionService {
 
     async getTransaction(hash :string): Promise<ITransaction | undefined> {
         const path = require("path");
-        //found if the hash is in the current node
+        
         let hashPath: string = "../"+Utilty.TRANSACTIONS_PATH();
         let hfiles: string[];
-        await readdir(path.resolve(__dirname, hashPath), async (err, files) => { 
-            
-            hfiles = files;
-            if(err) console.log(err); 
-
-            console.log(hashPath, files);
-
-            if(hfiles != null) {
-                for(let file of hfiles) {
-                    if(hash === file.split('.')[0]){
-                        return Promise.resolve(require(`../${Utilty.TRANSACTIONS_PATH()}${file}`));
-                    }
+        hfiles = readdirSync(path.resolve(__dirname, hashPath));
+        //found if the hash is in the current node
+        if(hfiles != null) {
+            for(let file of hfiles) {
+                if(hash === file.split('.')[0]){
+                    return Promise.resolve(require(`../${Utilty.TRANSACTIONS_PATH()}${file}`));
                 }
             }
-        
-            //if not in this node search in other nodes
-            //const nodesList: any[] = require(Utilty.NODES_LIST());
-            for(let node of nodes) {
-                axios.get(`${node.host}:${node.host}/transaction/${hash}`).then((data: any) => {
-                    if(data == null) return;
+        }
+    
+        //if not in this node search in other nodes
+        for(let node of nodes) {
+            axios.get(`${node.host}:${node.host}/transaction/${hash}`).then((data: any) => {
+                if(data == null) return;
 
-                    Utilty.fetchTransaction(data);
-                    return Promise.resolve(data);
-                });
-            }
-
-            return Promise.resolve(undefined);
-        });
-        return Promise.resolve(undefined);
+                Utilty.fetchTransaction(data);
+                return Promise.resolve(data);
+            });
+        }
     }
 }
